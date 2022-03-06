29acbab11114ff886e3915fb475ad973ec09e5fc
Sidney Cadot <sidney@jigsaw.nl>
2022-03-06 12:20:06 +0100

    Fixed test for C-style comments.

--- test_json_with_comments.py
+++ test_json_with_comments.py
@@ -2,24 +2,24 @@
 
 """A small example showing how to use the 'remove_comments_from_json_with_comments' function."""
 
-from json_with_comments import remove_comments_from_json_with_comments
-import json
+from json_with_comments import parse_json_with_comments
 
-stress_test = """# This is a small test to see if comment removal works as it should.
+stress_test = """// This is a small test to see if comment removal works as it should.
 {
-   "key"     : "value", # end-of-line comment...
+   "key"     : "value", // end-of-line comment...
    "hello"   : [ true, false, null, 1.0, "ciao" ],
    "strings" : ["", "Mötorhead", "/", "\\\\", "\\/", "\\\"", "🎈", "\\u2665" ],
    "numbers" : [0, -0, 0e0 ]
 }
 
-# bye!
+/* A block comment.
+
+tralala
+
+*/
+
 """
 
-print(stress_test)
-
-stress_test_json = remove_comments_from_json_with_comments(stress_test)
-
-stress_test_dict = json.loads(stress_test_json)
+stress_test_dict = parse_json_with_comments(stress_test)
 
 print(stress_test_dict)
