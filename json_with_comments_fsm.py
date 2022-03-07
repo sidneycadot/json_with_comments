c7bbcb21358565871513866025dc88183ccce58e
Sidney Cadot <sidney@jigsaw.nl>
2022-03-07 09:30:18 +0100

    Now overwriting comments, rather than deleting them (thanks Pep for the suggestion).

--- json_with_comments_fsm.py
+++ json_with_comments_fsm.py
@@ -3,25 +3,30 @@
 import json
 from enum import Enum
 
+
 class CharacterClass(Enum):
     """Character classes for parsing JSON-with-comments."""
-    FSLASH = 1
-    BSLASH = 2
-    QUOTE  = 3
-    CR     = 4
-    NL     = 5
-    STAR   = 6
-    OTHER  = 7
+    FSLASH = 1  # forward slash
+    BSLASH = 2  # back slash
+    QUOTE  = 3  # string quote
+    CR     = 4  # carriage return
+    NL     = 5  # newline
+    STAR   = 6  # star (asterisk)
+    OTHER  = 7  # any other character
+
 
 class FsmAction(Enum):
     """FSM actions for parsing JSON-with-comments."""
-    EMIT_NOTHING = 1
-    EMIT_CURRENT = 2
-    EMIT_FSLASH_THEN_CURRENT = 3
+    EMIT_CURRENT             = 1
+    EMIT_FSLASH_THEN_CURRENT = 2
+    EMIT_NOTHING             = 3
+    EMIT_ONE_SPACE           = 4
+    EMIT_TWO_SPACES          = 5
+
 
 class FsmState(Enum):
     """FSM states for parsing JSON-with-comments."""
-    START              = 1
+    DEFAULT            = 1
     MAYBE_COMMENT      = 2
     LINE_COMMENT       = 3
     BLOCK_COMMENT      = 4
@@ -29,64 +34,39 @@ class FsmState(Enum):
     STRING             = 6
     STRING_BACKSLASH   = 7
 
+
 _fsm_definition = {
 
-    (FsmState.START , CharacterClass.FSLASH) : (FsmAction.EMIT_NOTHING , FsmState.MAYBE_COMMENT),
-    (FsmState.START , CharacterClass.BSLASH) : (FsmAction.EMIT_CURRENT , FsmState.START        ),
-    (FsmState.START , CharacterClass.QUOTE ) : (FsmAction.EMIT_CURRENT , FsmState.STRING       ),
-    (FsmState.START , CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT , FsmState.START        ),
-    (FsmState.START , CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT , FsmState.START        ),
-    (FsmState.START , CharacterClass.STAR  ) : (FsmAction.EMIT_CURRENT , FsmState.START        ),
-    (FsmState.START , CharacterClass.OTHER ) : (FsmAction.EMIT_CURRENT , FsmState.START        ),
+    (FsmState.DEFAULT , CharacterClass.FSLASH) : (FsmAction.EMIT_NOTHING , FsmState.MAYBE_COMMENT),
+    (FsmState.DEFAULT , CharacterClass.QUOTE ) : (FsmAction.EMIT_CURRENT , FsmState.STRING       ),
+    (FsmState.DEFAULT , CharacterClass.OTHER ) : (FsmAction.EMIT_CURRENT , FsmState.DEFAULT      ),
 
-    (FsmState.MAYBE_COMMENT, CharacterClass.FSLASH) : (FsmAction.EMIT_NOTHING             , FsmState.LINE_COMMENT ),
-    (FsmState.MAYBE_COMMENT, CharacterClass.BSLASH) : (FsmAction.EMIT_FSLASH_THEN_CURRENT , FsmState.START        ),
-    (FsmState.MAYBE_COMMENT, CharacterClass.QUOTE ) : (FsmAction.EMIT_FSLASH_THEN_CURRENT , FsmState.START        ),
-    (FsmState.MAYBE_COMMENT, CharacterClass.CR    ) : (FsmAction.EMIT_FSLASH_THEN_CURRENT , FsmState.START        ),
-    (FsmState.MAYBE_COMMENT, CharacterClass.NL    ) : (FsmAction.EMIT_FSLASH_THEN_CURRENT , FsmState.START        ),
-    (FsmState.MAYBE_COMMENT, CharacterClass.STAR  ) : (FsmAction.EMIT_NOTHING             , FsmState.BLOCK_COMMENT),
-    (FsmState.MAYBE_COMMENT, CharacterClass.OTHER ) : (FsmAction.EMIT_FSLASH_THEN_CURRENT , FsmState.START        ),
+    (FsmState.MAYBE_COMMENT , CharacterClass.FSLASH) : (FsmAction.EMIT_TWO_SPACES          , FsmState.LINE_COMMENT ),
+    (FsmState.MAYBE_COMMENT , CharacterClass.STAR  ) : (FsmAction.EMIT_TWO_SPACES          , FsmState.BLOCK_COMMENT),
+    (FsmState.MAYBE_COMMENT , CharacterClass.OTHER ) : (FsmAction.EMIT_FSLASH_THEN_CURRENT , FsmState.DEFAULT      ),
 
-    (FsmState.LINE_COMMENT, CharacterClass.FSLASH) : (FsmAction.EMIT_NOTHING , FsmState.LINE_COMMENT ),
-    (FsmState.LINE_COMMENT, CharacterClass.BSLASH) : (FsmAction.EMIT_NOTHING , FsmState.LINE_COMMENT ),
-    (FsmState.LINE_COMMENT, CharacterClass.QUOTE ) : (FsmAction.EMIT_NOTHING , FsmState.LINE_COMMENT ),
-    (FsmState.LINE_COMMENT, CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT , FsmState.LINE_COMMENT ),
-    (FsmState.LINE_COMMENT, CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT , FsmState.START        ),
-    (FsmState.LINE_COMMENT, CharacterClass.STAR  ) : (FsmAction.EMIT_NOTHING , FsmState.LINE_COMMENT ),
-    (FsmState.LINE_COMMENT, CharacterClass.OTHER ) : (FsmAction.EMIT_NOTHING , FsmState.LINE_COMMENT ),
+    (FsmState.LINE_COMMENT , CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT   , FsmState.LINE_COMMENT ),
+    (FsmState.LINE_COMMENT , CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT   , FsmState.DEFAULT      ),
+    (FsmState.LINE_COMMENT , CharacterClass.OTHER ) : (FsmAction.EMIT_ONE_SPACE , FsmState.LINE_COMMENT ),
 
-    (FsmState.BLOCK_COMMENT, CharacterClass.FSLASH) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT, CharacterClass.BSLASH) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT, CharacterClass.QUOTE ) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT, CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT, CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT, CharacterClass.STAR  ) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT_STAR ),
-    (FsmState.BLOCK_COMMENT, CharacterClass.OTHER ) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT      ),
+    (FsmState.BLOCK_COMMENT , CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT   , FsmState.BLOCK_COMMENT      ),
+    (FsmState.BLOCK_COMMENT , CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT   , FsmState.BLOCK_COMMENT      ),
+    (FsmState.BLOCK_COMMENT , CharacterClass.STAR  ) : (FsmAction.EMIT_ONE_SPACE , FsmState.BLOCK_COMMENT_STAR ),
+    (FsmState.BLOCK_COMMENT , CharacterClass.OTHER ) : (FsmAction.EMIT_ONE_SPACE , FsmState.BLOCK_COMMENT      ),
 
-    (FsmState.BLOCK_COMMENT_STAR, CharacterClass.FSLASH) : (FsmAction.EMIT_NOTHING , FsmState.START              ),
-    (FsmState.BLOCK_COMMENT_STAR, CharacterClass.BSLASH) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT_STAR, CharacterClass.QUOTE ) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT_STAR, CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT_STAR, CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT , FsmState.BLOCK_COMMENT      ),
-    (FsmState.BLOCK_COMMENT_STAR, CharacterClass.STAR  ) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT_STAR ),
-    (FsmState.BLOCK_COMMENT_STAR, CharacterClass.OTHER ) : (FsmAction.EMIT_NOTHING , FsmState.BLOCK_COMMENT      ),
+    (FsmState.BLOCK_COMMENT_STAR , CharacterClass.FSLASH) : (FsmAction.EMIT_ONE_SPACE , FsmState.DEFAULT            ),
+    (FsmState.BLOCK_COMMENT_STAR , CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT   , FsmState.BLOCK_COMMENT      ),
+    (FsmState.BLOCK_COMMENT_STAR , CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT   , FsmState.BLOCK_COMMENT      ),
+    (FsmState.BLOCK_COMMENT_STAR , CharacterClass.STAR  ) : (FsmAction.EMIT_ONE_SPACE , FsmState.BLOCK_COMMENT_STAR ),
+    (FsmState.BLOCK_COMMENT_STAR , CharacterClass.OTHER ) : (FsmAction.EMIT_ONE_SPACE , FsmState.BLOCK_COMMENT      ),
 
-    (FsmState.STRING, CharacterClass.FSLASH) : (FsmAction.EMIT_CURRENT , FsmState.STRING           ),
-    (FsmState.STRING, CharacterClass.BSLASH) : (FsmAction.EMIT_CURRENT , FsmState.STRING_BACKSLASH ),
-    (FsmState.STRING, CharacterClass.QUOTE ) : (FsmAction.EMIT_CURRENT , FsmState.START            ),
-    (FsmState.STRING, CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT , FsmState.STRING           ),
-    (FsmState.STRING, CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT , FsmState.STRING           ),
-    (FsmState.STRING, CharacterClass.STAR  ) : (FsmAction.EMIT_CURRENT , FsmState.STRING           ),
-    (FsmState.STRING, CharacterClass.OTHER ) : (FsmAction.EMIT_CURRENT , FsmState.STRING           ),
+    (FsmState.STRING , CharacterClass.BSLASH ) : (FsmAction.EMIT_CURRENT , FsmState.STRING_BACKSLASH ),
+    (FsmState.STRING , CharacterClass.QUOTE  ) : (FsmAction.EMIT_CURRENT , FsmState.DEFAULT          ),
+    (FsmState.STRING , CharacterClass.OTHER  ) : (FsmAction.EMIT_CURRENT , FsmState.STRING           ),
 
-    (FsmState.STRING_BACKSLASH, CharacterClass.FSLASH) : (FsmAction.EMIT_CURRENT , FsmState.STRING ),
-    (FsmState.STRING_BACKSLASH, CharacterClass.BSLASH) : (FsmAction.EMIT_CURRENT , FsmState.STRING ),
-    (FsmState.STRING_BACKSLASH, CharacterClass.QUOTE ) : (FsmAction.EMIT_CURRENT , FsmState.STRING ),
-    (FsmState.STRING_BACKSLASH, CharacterClass.CR    ) : (FsmAction.EMIT_CURRENT , FsmState.STRING ),
-    (FsmState.STRING_BACKSLASH, CharacterClass.NL    ) : (FsmAction.EMIT_CURRENT , FsmState.STRING ),
-    (FsmState.STRING_BACKSLASH, CharacterClass.STAR  ) : (FsmAction.EMIT_CURRENT , FsmState.STRING ),
-    (FsmState.STRING_BACKSLASH, CharacterClass.OTHER ) : (FsmAction.EMIT_CURRENT , FsmState.STRING )
+    (FsmState.STRING_BACKSLASH, CharacterClass.OTHER    ) : (FsmAction.EMIT_CURRENT , FsmState.STRING )
 }
+
 
 _character_classifications = {
     '/'  : CharacterClass.FSLASH,
@@ -98,44 +78,85 @@ _character_classifications = {
 }
 
 
-def remove_comments_from_json_with_comments(s: str) -> str:
-    """Go through a state machine to remove line and block comments from JSON-with-comments."""
+def remove_comments_from_json_with_comments(input_string: str) -> str:
+    """Go through a state machine to remove line and block comments from JSON-with-comments.
+
+    Note: the comments are not actually removed; their contents are overwritten by spaces,
+    and carriage returns and line feeds inside comments are passed through as-is.
+
+    The reason for doing this is that it preserves line and character numbering of the output
+    relative to the input. This means that if the JSON parser that runs on our output needs
+    to report an issue, it can do so with line and character numbers that are meaningful.
+    """
+
     output = []
-    state = FsmState.START
-    for current_character in s:
+    state = FsmState.DEFAULT
+    for current_character in input_string:
+
         character_class = _character_classifications.get(current_character, CharacterClass.OTHER)
+
+        # If a (state, character_class) tuple is not explicitly handled in the FSM definition,
+        # we will follow the behavior of CharacterClass.OTHER.
+        if (state, character_class) not in _fsm_definition:
+            character_class = CharacterClass.OTHER
+
         (action, state) = _fsm_definition[(state, character_class)]
+
+        # Perform the specified action.
 
         if action == FsmAction.EMIT_FSLASH_THEN_CURRENT:
             output.append("/")
-        if action != FsmAction.EMIT_NOTHING:
+
+        if action in (FsmAction.EMIT_CURRENT, FsmAction.EMIT_FSLASH_THEN_CURRENT):
             output.append(current_character)
 
-    # The usual end state for a succesful FSM run should be START.
-    #
+        if action in (FsmAction.EMIT_ONE_SPACE, FsmAction.EMIT_TWO_SPACES):
+            output.append(" ")
+
+        if action == FsmAction.EMIT_TWO_SPACES:
+            output.append(" ")
+
+    # We're at the end of the character processing loop.
+
+    # Check if we've suppressed a forward slash in anticipation of a comment start. If so, output it.
+
+    # The usual end state for a succesful FSM run should be DEFAULT.
     # We also accept LINE_COMMENT, i.e., line comments that are not terminated by a newline are accepted.
     #
-    # The other 5 end states indicate some kind of error.
-    #
-    # The three states (MAYBE_COMMENT, STRING, STRING_BACKSLASH) will be intercepted by the JSON parser
-    # that runs on our output. We don't handle them here.
-    #
-    # For the two states (BLOCK_COMMENT, BLOCK_COMMENT_STAR), we raise a ValueError exception here.
-    #
+    # If we're in one of the five other possible states at the end, it indicates some parsing issue.
+
+    # The end states STRING and STRING_BACKSLASH indicate that the input ended while inside a string.
+    # This issue remains in the output, and will be caught by the JSON parser that runs on our output.
+
+    # If we're in the MAYBE_COMMENT state, the input ended in a forward slash. This forward slash was not
+    # emitted when we entered the MAYBE_COMMENT state, so we emit it now.
+    # This will be a JSON grammar error that will be caught by the JSON parser that runs on our output.
+
+    if state == FsmState.MAYBE_COMMENT:
+        output.append("/")
+
+    # The two remaining possible end states (BLOCK_COMMENT, BLOCK_COMMENT_STAR) indicate that the input
+    # ended while parsing an unterminated block comment.
+    # This issue will not be picked up by the JSON parser that runs on our output, so we handle it here:
+
     if state in (FsmState.BLOCK_COMMENT, FsmState.BLOCK_COMMENT_STAR):
         raise ValueError("Unterminated block comment at end of string.")
 
-    return "".join(output)
+    # Concatenate the output characters and return the result.
+
+    output_string = "".join(output)
+
+    return output_string
 
 
 def parse_json_with_comments(json_with_comments: str):
-    """Parse JSON-with-comments by removing the comments."""
+    """Parse JSON-with-comments by overwriting with whitespace."""
     json_without_comments = remove_comments_from_json_with_comments(json_with_comments)
     return json.loads(json_without_comments)
 
 
 def read_json_with_comments(filename: str):
-    """Read JSON-with-comments from file."""
+    """Read JSON-with-comments from file and parse it."""
     with open(filename, "r") as fi:
         json_with_comments = fi.read()
 
